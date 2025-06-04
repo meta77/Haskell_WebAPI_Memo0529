a02ff200e738e9d75131ea7ab9ba653c1ac8ed39
Meta77 <horokusa@keio.jp>
2025-06-04 21:46:49 +0900

    コメント　b :: ByteString

--- MemoApp0529/src/MemoApp0529/Handler.hs
+++ MemoApp0529/src/MemoApp0529/Handler.hs
@@ -29,8 +29,11 @@ getConn = liftIO $ open dbFile
 -- POST /memos : 新しいメモを作成
 createMemoHandler :: ActionM ()
 createMemoHandler = do
-  b <- body -- body は、HTTPリクエストの「中身」（JSONなど）を取得する関数。
-  case eitherDecode b :: Either String NewMemo of -- eitherDecode は JSON の文字列（ByteString）を Haskell の型に変換する関数。Either String NewMemo は「変換に成功すれば Right NewMemo、失敗すれば Left String（エラー文字列）」を返す、という型注釈です。
+  b <- body -- body は、HTTPリクエストの「中身」（JSONなど）を取得する関数。b :: ByteString
+  case eitherDecode b :: Either String NewMemo of -- eitherDecode は JSON の文字列（ByteString）を Haskell の型に変換する関数。
+  -- Either String NewMemo は「変換に成功すれば Right NewMemo、失敗すれば Left String（エラー文字列）」を返す、という型注釈です。
+
+
     Left err -> do -- JSONパースに失敗した場合
       status status400
       json $ object ["error" .= ("Invalid JSON: " ++ err)]
