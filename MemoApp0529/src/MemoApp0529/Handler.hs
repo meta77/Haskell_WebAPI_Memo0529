8e6f140e8ededfb59a0a3a57b92f6cf010771254
Meta77 <horokusa@keio.jp>
2025-06-04 21:50:49 +0900

    コメント　型注釈

--- MemoApp0529/src/MemoApp0529/Handler.hs
+++ MemoApp0529/src/MemoApp0529/Handler.hs
@@ -31,7 +31,7 @@ createMemoHandler :: ActionM ()
 createMemoHandler = do
   b <- body -- body は、HTTPリクエストの「中身」（JSONなど）を取得する関数。b :: ByteString
   case eitherDecode b :: Either String NewMemo of -- eitherDecode は JSON の文字列（ByteString）を Haskell の型に変換する関数。
-  -- Either String NewMemo は「変換に成功すれば Right NewMemo、失敗すれば Left String（エラー文字列）」を返す、という型注釈です。
+  -- :: Either String NewMemo は「変換に成功すれば Right NewMemo、失敗すれば Left String（エラー文字列）」を返す、という "型注釈 :: "です。
 
 
     Left err -> do -- JSONパースに失敗した場合
